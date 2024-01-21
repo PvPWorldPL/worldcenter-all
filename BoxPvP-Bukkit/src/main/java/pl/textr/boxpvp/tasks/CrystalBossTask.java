1da889b53753b73dc834bebb9129c00ff44cdfa4
PvPWorldPL <mrfokusssdraogniarz@gmail.com>
2024-01-21 02:08:43 +0100

    update GOWNO ALL DLA WRZYSTKICH

--- BoxPvP-Bukkit/src/main/java/pl/textr/boxpvp/tasks/CrystalBossTask.java
+++ BoxPvP-Bukkit/src/main/java/pl/textr/boxpvp/tasks/CrystalBossTask.java
@@ -21,7 +21,7 @@ import java.util.Calendar;
 
 public class CrystalBossTask implements Runnable {
     private static final BossBar ENDERMAN_BOSS_BAR_ACTIVE = Bukkit.createBossBar(ChatUtil.translateHexColorCodes("&aAktualnie trwa boss MIREK"), BarColor.GREEN, BarStyle.SOLID, BarFlag.values());
-    private static final BossBar ENDERMAN_BOSS_BAR = Bukkit.createBossBar(ChatUtil.translateHexColorCodes("&7Za &f1h 30sek pojawi się boss MIREK na strefie PvP"), BarColor.RED, BarStyle.SEGMENTED_10, BarFlag.values());
+    private static final BossBar ENDERMAN_BOSS_BAR = Bukkit.createBossBar(ChatUtil.translateHexColorCodes("&7Za &f1h 30sek pojawi się boss MIREK na strefie PvP"), BarColor.PURPLE, BarStyle.SEGMENTED_10, BarFlag.values());
 
     private static final int INITIAL_COUNTDOWN = 5400;
     private int currentCountdown = INITIAL_COUNTDOWN;
