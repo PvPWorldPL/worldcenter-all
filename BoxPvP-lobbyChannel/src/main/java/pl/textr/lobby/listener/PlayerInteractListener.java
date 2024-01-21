52f165a3a0a5c2c7961b6e2aed35f63329b838f7
PvPWorldPL <mrfokusssdraogniarz@gmail.com>
2024-01-21 19:20:34 +0100

    update tasks

--- BoxPvP-lobbyChannel/src/main/java/pl/textr/lobby/listener/PlayerInteractListener.java
+++ BoxPvP-lobbyChannel/src/main/java/pl/textr/lobby/listener/PlayerInteractListener.java
@@ -1,4 +1,4 @@
-	package pl.textr.lobby.listener;
+package pl.textr.lobby.listener;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -7,7 +7,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-
 import pl.textr.lobby.Lobby;
 
 public class PlayerInteractListener implements Listener {
@@ -15,19 +14,18 @@ public class PlayerInteractListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
         Player p = e.getPlayer();
-        ItemStack item = p.getItemInHand();
+        ItemStack item = p.getInventory().getItemInMainHand();
 
-        if (e.getAction() == Action.RIGHT_CLICK_AIR | e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (item != null) {
-                if (item.getType() == Material.getMaterial(Lobby.getInstance().cfg.getString("hotbarItem.material")) && item
-                        .getItemMeta().getDisplayName().equals(Lobby.getInstance().getString("hotbarItem.displayname"))) {
-                    e.setCancelled(true);
-                    Lobby.getInstance().openGUI(p);
-                    return;
-                }
+        if ((e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) && !item.getType().isAir()) {
+            String materialName = Lobby.getInstance().cfg.getString("hotbarItem.material");
+            String displayName = Lobby.getInstance().getString("hotbarItem.displayname");
+
+            if (materialName != null && displayName != null && item.getType() == Material.getMaterial(materialName) &&
+                    item.hasItemMeta() && item.getItemMeta().hasDisplayName() &&
+                    item.getItemMeta().getDisplayName().equals(displayName)) {
+                e.setCancelled(true);
+                Lobby.getInstance().openGUI(p);
             }
         }
-
     }
-
 }
