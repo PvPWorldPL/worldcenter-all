826a771adb561b80d84cc16cd402f9ca245fc5a7
PvPWorldPL <mrfokusssdraogniarz@gmail.com>
2024-01-25 23:24:22 +0100

    update tasks

--- BoxPvP-Bukkit/src/main/java/pl/textr/boxpvp/tasks/BossBarTask.java
+++ BoxPvP-Bukkit/src/main/java/pl/textr/boxpvp/tasks/BossBarTask.java
@@ -25,28 +25,31 @@ public class BossBarTask implements Runnable {
         long turbodropTimer = Main.getPlugin().getConfiguration().turbodrop;
         long turboRangiTimer = Main.getPlugin().getConfiguration().vouchery;
 
-        Bukkit.getOnlinePlayers().forEach(player -> {
-            turboDropBossBar.addPlayer(player);
-            turboRangiBossBar.addPlayer(player);
-        });
-
-
         if (turbodropTimer < System.currentTimeMillis()) {
-            turboDropBossBar.setTitle(ChatColor.translateAlternateColorCodes('&', "&eɴᴀ sᴇʀᴡᴇʀᴢᴇ ᴊᴇsᴛ ᴀᴋᴜᴛᴀʟɴɪᴇ &6ᴅʀᴏᴘ x" + Main.getPlugin().getConfiguration().turbodropmnoznik() + " &8(&f" + DataUtil.secondsToString(turbodropTimer) + "&8)"));
-            double progress = 1.0 - ((double) (System.currentTimeMillis() - turbodropTimer) / (double) (turbodropTimer - System.currentTimeMillis()));
-            turboDropBossBar.setProgress(Math.max(0.0, Math.min(1.0, progress)));
+            if (!turboDropBossBar.getPlayers().isEmpty()) {
+                turboDropBossBar.removeAll();
+            }
         } else {
-            turboDropBossBar.setProgress(1.0);
-            turboDropBossBar.removeAll();
-        }
+            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                turboDropBossBar.addPlayer(onlinePlayer);
+                turboDropBossBar.setTitle(ChatColor.translateAlternateColorCodes('&', "&eɴᴀ sᴇʀᴡᴇʀᴢᴇ ᴊᴇsᴛ ᴀᴋᴜᴛᴀʟɴɪᴇ &6ᴅʀᴏᴘ x" + Main.getPlugin().getConfiguration().turbodropmnoznik() + " &8(&f" + DataUtil.secondsToString(turbodropTimer) + "&8)"));
+                double progress = 1.0 - ((double) (System.currentTimeMillis() - turbodropTimer) / (double) (turbodropTimer - System.currentTimeMillis()));
+                turboDropBossBar.setProgress(Math.max(0.0, Math.min(1.0, progress)));
+            }
 
-        if (turboRangiTimer < System.currentTimeMillis()) {
-            turboRangiBossBar.setTitle(ChatColor.translateAlternateColorCodes('&', "&eɴᴀ sᴇʀᴡᴇʀᴢᴇ ᴊᴇsᴛ ᴀᴋᴜᴛᴀʟɴɪᴇ &6ᴅʀᴏᴘ ʀᴀɴɢ &8(&f" + DataUtil.secondsToString(turboRangiTimer) + "&8)"));
-            double progress = 1.0 - ((double) (System.currentTimeMillis() - turboRangiTimer) / (double) (turboRangiTimer - System.currentTimeMillis()));
-            turboRangiBossBar.setProgress(Math.max(0.0, Math.min(1.0, progress)));
-        } else {
-            turboRangiBossBar.setProgress(1.0);
-            turboRangiBossBar.removeAll();
+            if (turboRangiTimer < System.currentTimeMillis()) {
+                if (!turboRangiBossBar.getPlayers().isEmpty()) {
+                    turboRangiBossBar.removeAll();
+                }
+            } else {
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    turboRangiBossBar.addPlayer(onlinePlayer);
+                    turboRangiBossBar.setTitle(ChatColor.translateAlternateColorCodes('&', "&eɴᴀ sᴇʀᴡᴇʀᴢᴇ ᴊᴇsᴛ ᴀᴋᴜᴛᴀʟɴɪᴇ &6ᴅʀᴏᴘ ʀᴀɴɢ &8(&f" + DataUtil.secondsToString(turboRangiTimer) + "&8)"));
+                    double progress = 1.0 - ((double) (System.currentTimeMillis() - turboRangiTimer) / (double) (turboRangiTimer - System.currentTimeMillis()));
+                    turboDropBossBar.setProgress(Math.max(0.0, Math.min(1.0, progress)));
+                }
+
+            }
         }
     }
 }
