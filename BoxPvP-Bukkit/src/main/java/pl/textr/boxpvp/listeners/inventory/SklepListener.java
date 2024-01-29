aa2972f4393a9cc2573f064637b519a9e05ddede
PvPWorldPL <mrfokusssdraogniarz@gmail.com>
2024-01-29 12:44:02 +0100

    feat: /sklep monety dodane

--- BoxPvP-Bukkit/src/main/java/pl/textr/boxpvp/listeners/inventory/SklepListener.java
+++ BoxPvP-Bukkit/src/main/java/pl/textr/boxpvp/listeners/inventory/SklepListener.java
@@ -49,9 +49,9 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
     UserProfile user = UserAccountManager.getUser(p);
     ItemStack item = e.getCurrentItem();
     if (item != null) {
-        if (item.getType() == Material.GHAST_TEAR) {
+        if (item.getType() == Material.LIME_DYE) {
             if (user.getMoney() >= 1000) {
-                ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&aPomyślnie zakupiono &#00F3FF&lK&#00E9FF&lr&#00DFFF&lo&#00D5FF&lp&#00CBFF&ll&#00C2FF&la &#00B8FF&lb&#00AEFF&lo&#00A4FF&lg&#009AFF&la"));
+                ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&aPomyślnie zakupiono &#30ff38&lʀ&#2ef636&lᴜ&#2cec34&lʙ&#2ae331&lɪ&#28d92f&lɴ &#26d02d&ls&#24c62b&lᴢ&#22bd29&lᴍ&#21b326&lᴀ&#1faa24&lʀ&#1da022&lᴀ&#1b9720&lɢ&#198d1e&lᴅ&#17841b&lᴏ&#157a19&lᴡ&#137117&lʏ &8(&6✪1&8)"));
                 user.setMoney(user.getMoney() - 1000);
                 p.getInventory().addItem(new ItemStack(ItemsManager.getMoneta1(10)));
                 p.closeInventory();
@@ -62,9 +62,9 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
             return;
         }
 
-        if (item.getType() == Material.WITHER_ROSE) {
+        if (item.getType() == Material.BLUE_DYE) {
             if (user.getMoney() >= 5000) {
-                ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &#DE00FF&lP&#CC01ED&lo&#BA02DA&ls&#A903C8&lk&#9704B5&lr&#8505A3&la&#730591&lm&#61067E&li&#50076C&la&#3E0859&lc&#2C0947&lz"));
+                ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &#1400fb&lʀ&#1803f9&lᴜ&#1b06f8&lʙ&#1f09f6&lɪ&#220cf5&lɴ &#260ff3&lɢ&#2912f1&lʀ&#2d15f0&lᴀ&#3018ee&lɴ&#341bec&lᴀ&#371eeb&lᴛ&#3b21e9&lᴏ&#3e24e8&lᴡ&#4227e6&lʏ &8(&6✪4&8)"));
                 user.setMoney(user.getMoney() - 5000);
                 p.getInventory().addItem(new ItemStack(ItemsManager.getMoneta4(5)));
                 p.closeInventory();
@@ -75,9 +75,9 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
             return;
         }
 
-        if (item.getType() == Material.LEAD) {
+        if (item.getType() == Material.BLUE_DYE) {
             if (user.getMoney() >= 3000) {
-                ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &#DE00FF&lP&#CC01ED&lo&#BA02DA&ls&#A903C8&lk&#9704B5&lr&#8505A3&la&#730591&lm&#61067E&li&#50076C&la&#3E0859&lc&#2C0947&lz"));
+                ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &#5be1ff&lʀ&#56d9fb&lᴜ&#51d0f6&lʙ&#4cc8f2&lɪ&#46c0ed&lɴ &#41b8e9&lʟ&#3cafe4&lᴀ&#37a7e0&lᴢ&#329fdb&lᴜ&#2d96d7&lʀ&#288ed2&lʏ&#2286ce&lᴛ&#1d7ec9&lᴏ&#1875c5&lᴡ&#136dc0&lʏ &8(&6✪2&8)"));
                 user.setMoney(user.getMoney() - 3000);
                 p.getInventory().addItem(new ItemStack(ItemsManager.getMoneta2(5)));
                 p.closeInventory();
@@ -158,11 +158,11 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
       ItemStack item = e.getCurrentItem();
       UserProfile user = UserAccountManager.getUser(p);
       if (item != null) {
-        if (item.getType() == Material.GHAST_TEAR) {
+        if (item.getType() == Material.LIME_DYE) {
           if (e.getClick() == ClickType.LEFT) {
             if (user.hasEnough(BigDecimal.valueOf(1L))) {
               user.removeBalance(1.0D);
-              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#00F3FF&lK&#00E9FF&lr&#00DFFF&lo&#00D5FF&lp&#00CBFF&ll&#00C2FF&la &#00B8FF&lb&#00AEFF&lo&#00A4FF&lg&#009AFF&la"));
+              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#30ff38&lʀ&#2ef636&lᴜ&#2cec34&lʙ&#2ae331&lɪ&#28d92f&lɴ &#26d02d&ls&#24c62b&lᴢ&#22bd29&lᴍ&#21b326&lᴀ&#1faa24&lʀ&#1da022&lᴀ&#1b9720&lɢ&#198d1e&lᴅ&#17841b&lᴏ&#157a19&lᴡ&#137117&lʏ &8(&6✪1&8)"));
               p.getInventory().addItem(new ItemStack(ItemsManager.getMoneta1(1)));
               ShopMenu.show2(p);
             } else {
@@ -176,7 +176,7 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
           if (e.getClick() == ClickType.RIGHT) {
             if (p.getInventory().containsAtLeast(ItemsManager.getMoneta1(1), 1)) {
               user.addBalance(1.0D);
-              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#00F3FF&lK&#00E9FF&lr&#00DFFF&lo&#00D5FF&lp&#00CBFF&ll&#00C2FF&la &#00B8FF&lb&#00AEFF&lo&#00A4FF&lg&#009AFF&la"));
+              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#30ff38&lʀ&#2ef636&lᴜ&#2cec34&lʙ&#2ae331&lɪ&#28d92f&lɴ &#26d02d&ls&#24c62b&lᴢ&#22bd29&lᴍ&#21b326&lᴀ&#1faa24&lʀ&#1da022&lᴀ&#1b9720&lɢ&#198d1e&lᴅ&#17841b&lᴏ&#157a19&lᴡ&#137117&lʏ &8(&6✪1&8)"));
              
               p.getInventory().removeItem(new ItemStack(ItemsManager.getMoneta1(1))); 
               ShopMenu.show2(p);
@@ -188,18 +188,18 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
           } 
           if (e.getClick().equals(ClickType.SHIFT_RIGHT) || e.getClick().equals(ClickType.SHIFT_LEFT) || e.getClick().equals(ClickType.DOUBLE_CLICK)) {
           
-        	  ItemsManager.kasakasa(p, ItemsManager.getMoneta1(1), 1.0D);
+        	  ItemsManager.convertMoneyToCurrency(p, ItemsManager.getMoneta1(1), 1.0D);
             ShopMenu.show2(p);
           } 
           return;
         } 
         
         
-        if (item.getType() == Material.LEAD) {
+        if (item.getType() == Material.LIGHT_BLUE_DYE) {
           if (e.getClick() == ClickType.LEFT) {
             if (user.hasEnough(BigDecimal.valueOf(64L))) {
               user.removeBalance(64.0D);
-              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#DE00FF&lP&#CC01ED&lo&#BA02DA&ls&#A903C8&lk&#9704B5&lr&#8505A3&la&#730591&lm&#61067E&li&#50076C&la&#3E0859&lc&#2C0947&lz"));
+              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#5be1ff&lʀ&#56d9fb&lᴜ&#51d0f6&lʙ&#4cc8f2&lɪ&#46c0ed&lɴ &#41b8e9&lʟ&#3cafe4&lᴀ&#37a7e0&lᴢ&#329fdb&lᴜ&#2d96d7&lʀ&#288ed2&lʏ&#2286ce&lᴛ&#1d7ec9&lᴏ&#1875c5&lᴡ&#136dc0&lʏ &8(&6✪2&8)"));
               p.getInventory().addItem(new ItemStack(ItemsManager.getMoneta2(1)));
               ShopMenu.show2(p);
             } else {
@@ -212,7 +212,7 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
           if (e.getClick() == ClickType.RIGHT) {
             if (p.getInventory().containsAtLeast(ItemsManager.getMoneta2(1), 1)) {
               user.addBalance(64.0D);
-              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#DE00FF&lP&#CC01ED&lo&#BA02DA&ls&#A903C8&lk&#9704B5&lr&#8505A3&la&#730591&lm&#61067E&li&#50076C&la&#3E0859&lc&#2C0947&lz"));
+              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#5be1ff&lʀ&#56d9fb&lᴜ&#51d0f6&lʙ&#4cc8f2&lɪ&#46c0ed&lɴ &#41b8e9&lʟ&#3cafe4&lᴀ&#37a7e0&lᴢ&#329fdb&lᴜ&#2d96d7&lʀ&#288ed2&lʏ&#2286ce&lᴛ&#1d7ec9&lᴏ&#1875c5&lᴡ&#136dc0&lʏ &8(&6✪2&8)"));
             
               p.getInventory().removeItem(new ItemStack(ItemsManager.getMoneta2(1)));
               ShopMenu.show2(p);
@@ -224,7 +224,7 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
           } 
           if (e.getClick().equals(ClickType.SHIFT_RIGHT) || e.getClick().equals(ClickType.SHIFT_LEFT) || e.getClick().equals(ClickType.DOUBLE_CLICK)) {
           
-        	  ItemsManager.kasakasa(p, ItemsManager.getMoneta2(1), 64.0D);
+        	  ItemsManager.convertMoneyToCurrency(p, ItemsManager.getMoneta2(1), 64.0D);
             ShopMenu.show2(p);
           } 
           return;
@@ -232,11 +232,11 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
         
         
         
-        if (item.getType() == Material.CRIMSON_FUNGUS) {
+        if (item.getType() == Material.MAGENTA_DYE) {
           if (e.getClick() == ClickType.LEFT) {
             if (user.hasEnough(BigDecimal.valueOf(4000L))) {
               user.removeBalance(4000.1D);
-              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#C03450&lR&#BB3350&lu&#B53250&lb&#B03151&li&#AA3051&ln&#A52F51&lk&#A02E51&lo&#9A2D52&lw&#952C52&ly &#902B52&lg&#8A2A52&lr&#852953&lz&#7F2853&ly&#7A2753&lb"));
+              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#fa1eff&lʀ&#f21ffb&lᴜ&#eb20f7&lʙ&#e321f2&lɪ&#dc22ee&lɴ &#d423ea&lʙ&#cc24e6&lᴜ&#c525e2&lʀ&#bd27dd&ls&#b628d9&lᴢ&#ae29d5&lᴛ&#a62ad1&lʏ&#9f2bcd&lɴ&#972cc8&lᴏ&#902dc4&lᴡ&#882ec0&lʏ  &8(&6✪3&8)"));
               p.getInventory().addItem(new ItemStack(ItemsManager.getMoneta3(1)));
               ShopMenu.show2(p);
             } else {
@@ -249,7 +249,7 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
           if (e.getClick() == ClickType.RIGHT) {
             if (p.getInventory().containsAtLeast(ItemsManager.getMoneta3(1), 1)) {
               user.addBalance(4000.1D);
-              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#C03450&lR&#BB3350&lu&#B53250&lb&#B03151&li&#AA3051&ln&#A52F51&lk&#A02E51&lo&#9A2D52&lw&#952C52&ly &#902B52&lg&#8A2A52&lr&#852953&lz&#7F2853&ly&#7A2753&lb"));
+              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#fa1eff&lʀ&#f21ffb&lᴜ&#eb20f7&lʙ&#e321f2&lɪ&#dc22ee&lɴ &#d423ea&lʙ&#cc24e6&lᴜ&#c525e2&lʀ&#bd27dd&ls&#b628d9&lᴢ&#ae29d5&lᴛ&#a62ad1&lʏ&#9f2bcd&lɴ&#972cc8&lᴏ&#902dc4&lᴡ&#882ec0&lʏ  &8(&6✪3&8)"));
             
               p.getInventory().removeItem(new ItemStack(ItemsManager.getMoneta3(1)));
               ShopMenu.show2(p);
@@ -261,18 +261,18 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
           } 
           if (e.getClick().equals(ClickType.SHIFT_RIGHT) || e.getClick().equals(ClickType.SHIFT_LEFT) || e.getClick().equals(ClickType.DOUBLE_CLICK)) {
            
-        	  ItemsManager.kasakasa(p, ItemsManager.getMoneta3(1), 4096.0D);
+        	  ItemsManager.convertMoneyToCurrency(p, ItemsManager.getMoneta3(1), 4096.0D);
             ShopMenu.show2(p);
           } 
           return;
         } 
         
         
-        if (item.getType() == Material.WITHER_ROSE) {
+        if (item.getType() == Material.BLUE_DYE) {
           if (e.getClick() == ClickType.LEFT) {
             if (user.hasEnough(BigDecimal.valueOf(262140000L))) {
               user.removeBalance(262140000D);    	   
-              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#FF7400&lR&#F87405&ló&#F1750B&lz&#EB7510&la &#E47615&lU&#DD761B&lp&#D67720&la&#CF7726&ld&#C8782B&lł&#C27830&le&#BB7936&lg&#B4793B&lo"));
+              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#1400fb&lʀ&#1803f9&lᴜ&#1b06f8&lʙ&#1f09f6&lɪ&#220cf5&lɴ &#260ff3&lɢ&#2912f1&lʀ&#2d15f0&lᴀ&#3018ee&lɴ&#341bec&lᴀ&#371eeb&lᴛ&#3b21e9&lᴏ&#3e24e8&lᴡ&#4227e6&lʏ &8(&6✪4&8)"));
               p.getInventory().addItem(new ItemStack(ItemsManager.getMoneta4(1)));
               ShopMenu.show2(p);
             } else {
@@ -285,7 +285,7 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
           if (e.getClick() == ClickType.RIGHT) {
             if (p.getInventory().containsAtLeast(ItemsManager.getMoneta4(1), 1)) {
               user.addBalance(262140000D);
-              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#FF7400&lR&#F87405&ló&#F1750B&lz&#EB7510&la &#E47615&lU&#DD761B&lp&#D67720&la&#CF7726&ld&#C8782B&lł&#C27830&le&#BB7936&lg&#B4793B&lo"));
+              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#1400fb&lʀ&#1803f9&lᴜ&#1b06f8&lʙ&#1f09f6&lɪ&#220cf5&lɴ &#260ff3&lɢ&#2912f1&lʀ&#2d15f0&lᴀ&#3018ee&lɴ&#341bec&lᴀ&#371eeb&lᴛ&#3b21e9&lᴏ&#3e24e8&lᴡ&#4227e6&lʏ &8(&6✪4&8)"));
 	           p.getInventory().removeItem(new ItemStack(ItemsManager.getMoneta4(1)));
               ShopMenu.show2(p);
             } else {
@@ -296,19 +296,19 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
 	          } 
 	          if (e.getClick().equals(ClickType.SHIFT_RIGHT) || e.getClick().equals(ClickType.SHIFT_LEFT) || e.getClick().equals(ClickType.DOUBLE_CLICK)) {
 	          
-	        	  ItemsManager.kasakasa(p, ItemsManager.getMoneta4(1), 262144.0D);
+	        	  ItemsManager.convertMoneyToCurrency(p, ItemsManager.getMoneta4(1), 262144.0D);
 	            ShopMenu.show2(p);
 	          } 
 	          return;
 	        } 
 	        
 	        
-	        if (item.getType() == Material.HORN_CORAL) {
+	        if (item.getType() == Material.RED_DYE) {
 	          if (e.getClick() == ClickType.LEFT) {
 	            if (user.hasEnough(BigDecimal.valueOf(562140000L))) {
 	              user.removeBalance(562140000D);
  
-	              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#FFF000&lK&#FCE902&lo&#F9E205&lr&#F6DB07&la&#F2D40A&ll&#EFCD0C&lo&#ECC60F&lw&#E9BE11&ly &#E6B714&lp&#E3B016&lr&#DFA919&lo&#DCA21B&lm&#D99B1E&ly&#D69420&lk"));
+	              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie zakupiono &fx1 &#fb2020&lʀ&#fb2421&lᴜ&#fb2821&lʙ&#fb2d22&lɪ&#fc3122&lɴ &#fc3523&ls&#fc3923&lᴢ&#fc3e24&lᴋ&#fc4225&lᴀ&#fc4625&lʀ&#fc4a26&lʟ&#fd4e26&lᴀ&#fd5327&lᴛ&#fd5727&lɴ&#fd5b28&lʏ &8(&6✪5&8)"));
               p.getInventory().addItem(new ItemStack(ItemsManager.getMoneta5(1)));
               ShopMenu.show2(p);
             } else {
@@ -321,7 +321,7 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
           if (e.getClick() == ClickType.RIGHT) {
             if (p.getInventory().containsAtLeast(ItemsManager.getMoneta5(1), 1)) {
               user.addBalance(562140000D);
-              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#FFF000&lK&#FCE902&lo&#F9E205&lr&#F6DB07&la&#F2D40A&ll&#EFCD0C&lo&#ECC60F&lw&#E9BE11&ly &#E6B714&lp&#E3B016&lr&#DFA919&lo&#DCA21B&lm&#D99B1E&ly&#D69420&lk"));
+              ChatUtil.sendMessage(p, ChatUtil.translateHexColorCodes("&7Pomyslnie sprzedano &fx1 &#fb2020&lʀ&#fb2421&lᴜ&#fb2821&lʙ&#fb2d22&lɪ&#fc3122&lɴ &#fc3523&ls&#fc3923&lᴢ&#fc3e24&lᴋ&#fc4225&lᴀ&#fc4625&lʀ&#fc4a26&lʟ&#fd4e26&lᴀ&#fd5327&lᴛ&#fd5727&lɴ&#fd5b28&lʏ &8(&6✪5&8)"));
                p.getInventory().removeItem(new ItemStack(ItemsManager.getMoneta5(1)));
               ShopMenu.show2(p);
             } else {
@@ -332,7 +332,7 @@ if (ChatUtil.fixColor("&eSklep &7(sklep za czas)").equalsIgnoreCase(e.getView().
 	    	          } 
 	    	          if (e.getClick().equals(ClickType.SHIFT_RIGHT) || e.getClick().equals(ClickType.SHIFT_LEFT) || e.getClick().equals(ClickType.DOUBLE_CLICK)) {
 	    	        
-	    	        	  ItemsManager.kasakasa(p, ItemsManager.getMoneta5(1), 1.6777216E7D);
+	    	        	  ItemsManager.convertMoneyToCurrency(p, ItemsManager.getMoneta5(1), 1.6777216E7D);
 	    	            ShopMenu.show2(p);
 	    	          }
             }
